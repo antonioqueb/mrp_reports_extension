98b6de02543087378acab659e13885f9cd6e67fc
Antonio Queb <antonioqueb@alphaqueb.online>
2024-11-17 13:04:37 -0600

    Up

--- models/mrp_reports.py
+++ models/mrp_reports.py
@@ -8,6 +8,7 @@ class MrpReportEfficiency(models.Model):
     @api.depends('workorder_ids')
     def _compute_efficiency(self):
         for record in self:
-            planned_time = sum(record.workorder_ids.mapped('time_planned'))
+            # Usa el campo correcto para el tiempo planificado
+            planned_time = sum(record.workorder_ids.mapped('duration_expected'))
             actual_time = sum(record.workorder_ids.mapped('duration'))
             record.efficiency_percentage = (planned_time / actual_time) * 100 if actual_time else 0
