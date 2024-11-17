09209b6be80fdc3a07cc99215808e32d06827de1
Antonio Queb <antonioqueb@alphaqueb.online>
2024-11-17 17:33:22 -0600

    Up

--- models/mrp_reports.py
+++ models/mrp_reports.py
@@ -6,7 +6,7 @@ class MrpProduction(models.Model):
     efficiency_percentage = fields.Float(
         string="Eficiencia (%)",
         compute='_compute_efficiency',
-        store=True,  # Aseguramos que los datos se guarden
+        store=True,
     )
 
     @api.depends('workorder_ids.duration_expected', 'workorder_ids.duration')
@@ -15,8 +15,8 @@ class MrpProduction(models.Model):
         Calcula la eficiencia de la orden de fabricación basada en las órdenes de trabajo asociadas.
         """
         for record in self:
-            planned_time = sum(record.workorder_ids.mapped('duration_expected'))  # Duración planificada total
-            actual_time = sum(record.workorder_ids.mapped('duration'))  # Duración real total
+            planned_time = sum(record.workorder_ids.mapped('duration_expected'))
+            actual_time = sum(record.workorder_ids.mapped('duration'))
             if actual_time > 0:
                 record.efficiency_percentage = (planned_time / actual_time) * 100
             else:
