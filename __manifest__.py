6b0ebfa79d7f9f74db18134104f669714c77ebc5
Antonio Queb <antonioqueb@alphaqueb.online>
2024-11-17 16:22:01 -0600

    Up

--- __manifest__.py
+++ __manifest__.py
@@ -7,11 +7,12 @@
     'website': 'https://tuweb.com',
     'depends': ['mrp', 'base'],
     'data': [
-        'security/ir.model.access.csv',
-        'reports/report_templates.xml',
-        'reports/report_mrp_efficiency.xml',
-        'views/mrp_report_views.xml',
+    'security/ir.model.access.csv',
+    'reports/report_templates.xml',
+    'reports/report_mrp_efficiency.xml',
+    'views/mrp_report_views.xml',
     ],
+
 
     'installable': True,
     'application': False,
